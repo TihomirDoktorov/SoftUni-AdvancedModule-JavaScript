84d7f2062160b2bb1238e124e71963ab7346999d
Tihomir Doktorov <tihomirdoktorov@gmail.com>
2022-05-29 17:45:23 +0300

    Delete Arrays and Nested Arrays - Lab directory

--- Arrays and Nested Arrays - Lab/03. Sum First Last.js
+++
@@ -1,6 +0,0 @@
-function sum(arr) {
-  console.log(Number(arr.shift()) + Number(arr.pop()));
-}
-
-sum([`20`, `30`, `40`]);
-sum([`5`, `10`]);
--- Arrays and Nested Arrays - Lab/05. Smallest Two Numbers.js
+++
@@ -1,7 +0,0 @@
-function smalleest(arr) {
-  let sorted = arr.sort((a, b) => a - b);
-  console.log(sorted[0], ` `, sorted[1]);
-}
-
-smalleest([30, 15, 50, 5]);
-smalleest([3, 0, 10, 4, 7, 3]);
